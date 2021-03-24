a058d119d237679b33d2eaa03f96c4d2ae3ebeb7
bioback <78535998+bioback@users.noreply.github.com>
2021-03-24 16:15:33 +0100

    add badge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React,{useState} from 'react'
 import './App.css';
 import AlertDismissibleExample from './components/WarningSign'
+import MyBadge from './components/MyBadge'
+
 function App() {
   const [show, setShow] = useState(true);
   return (
     <div className="App">
       <AlertDismissibleExample  show={show} setShow={setShow}  msg='this is an alert'/>
+      <MyBadge text="Random" color="red"/>
     </div>
   );
 }
